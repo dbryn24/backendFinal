52dc621253b3752facd3b7e58d8894cd12e56d99
Thimothy25 <thimothypongantung27@gmail.com>
2025-05-07 22:54:31 +0800

    adding jwt and fix login

--- Frontend/src/services/product.js
+++
@@ -1,5 +0,0 @@
-import api from "./api";
-
-export const fetchCategories = () => api.get("/categories");
-export const fetchProducts = (categoryId) =>
-  api.get("/products", { params: { category: categoryId } });
